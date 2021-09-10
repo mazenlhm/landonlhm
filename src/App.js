d738e0fcd16da3c64e023adc5404881dfddbd0a0
Mazen LAHHAM <mazenlhm@hotmail.com>
2021-09-10 23:40:34 +0300

    modi app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React in one day only
+                    Learn React easiy
                 </a>
             </header>
         </div>
